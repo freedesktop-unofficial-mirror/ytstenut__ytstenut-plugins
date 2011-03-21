21c2874e4536bd679e2aa7b6caad9c0e8483c310
Stef Walter <stefw@collabora.co.uk>
2011-03-21 16:42:32 +0100

    Revert "Fix salut plugin so it builds with new telepathy-salut support."
    This reverts commit 0738b43530fdadd8b22deeb7dee5321ea21e098b.

--- salut/plugin.h
+++ salut/plugin.h
@@ -1,25 +1,3 @@
-/*
- * plugin.h -- The salut plugin
- * Copyright (C) 2011 Collabora Ltd.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-#ifndef __YTSTENUT_PLUGIN_H__
-#define __YTSTENUT_PLUGIN_H__
-
 #include <glib-object.h>
 
 typedef struct _YtstenutPluginClass YtstenutPluginClass;
@@ -51,5 +29,3 @@ GType ytstenut_plugin_get_type (void);
 #define YTSTENUT_PLUGIN_GET_CLASS(obj) \
   (G_TYPE_INSTANCE_GET_CLASS ((obj), YTSTENUT_TYPE_PLUGIN, \
                               YtstenutPluginClass))
-
-#endif /* __YTSTENUT_PLUGIN_H__ */
