7e0321499f57134b77acf6cdaa7dde306d8e9b37
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-04-13 18:19:25 +0100

    yconstants: add more
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- tests/twisted/yconstants.py
+++ tests/twisted/yconstants.py
@@ -3,3 +3,22 @@ YTST_ACCOUNT_MANAGER_IFACE = "com.meego.xpmn.ytstenut.AccountManager"
 YTST_ACCOUNT_PATH = "/org/freedesktop/Telepathy/Account/salut/local_ytstenut/automatic_account"
 
 STATUS_IFACE = 'com.meego.xpmn.ytstenut.Status'
+
+CHANNEL_IFACE = 'com.meego.xpmn.ytstenut.Channel'
+
+REQUEST_TYPE = CHANNEL_IFACE + '.RequestType'
+REQUEST_ATTRIBUTES = CHANNEL_IFACE + '.RequestAttributes'
+REQUEST_BODY = CHANNEL_IFACE + '.RequestBody'
+TARGET_SERVICE = CHANNEL_IFACE + '.TargetService'
+INITIATOR_SERVICE = CHANNEL_IFACE + '.InitiatorService'
+
+REQUEST_TYPE_GET = 1
+REQUEST_TYPE_SET = 2
+
+ERROR_TYPE_CANCEL = 1
+ERROR_TYPE_CONTINUE = 2
+ERROR_TYPE_MODIFY = 3
+ERROR_TYPE_AUTH = 4
+ERROR_TYPE_WAIT = 5
+
+MESSAGE_NS = 'urn:ytstenut:message'
