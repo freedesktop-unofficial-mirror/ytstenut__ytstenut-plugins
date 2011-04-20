24e61aba49e850f7c944797bd2bf1eb401029249
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-04-20 15:51:08 +0100

    utils: add a capabilities namespace definition and more cleanups
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- salut/utils.h
+++ salut/utils.h
@@ -28,6 +28,7 @@ G_BEGIN_DECLS
 
 #define YTST_MESSAGE_NS "urn:ytstenut:message"
 #define YTST_STATUS_NS "urn:ytstenut:status"
+#define YTST_CAPABILITIES_NS "urn:ytstenut:capabilities"
 
 GQuark ytst_message_error_quark (void);
 #define YTST_MESSAGE_ERROR (ytst_message_error_quark ())
@@ -43,7 +44,8 @@ typedef enum {
   YTST_MESSAGE_ERROR_ITEM_NOT_FOUND,
 } YtstMessageError;
 
-#define CAPS_FEATURE_PREFIX "urn:ytstenut:capabilities:"
+#define CAPS_FEATURE_PREFIX YTST_CAPABILITIES_NS ":"
+#define SERVICE_PREFIX YTST_CAPABILITIES_NS "#"
 
 gint ytst_message_error_type_to_wocky (guint ytstenut_type);
 
