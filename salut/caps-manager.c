531545824c84f9eec336ed9ce6f9031ccdc01ee1
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-04-25 11:31:41 +0100

    caps-manager: create data forms out of nice HCTs
    And try and test it.
    
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- salut/caps-manager.c
+++ salut/caps-manager.c
@@ -26,6 +26,9 @@
 
 #include <telepathy-glib/channel-manager.h>
 
+#include <wocky/wocky-data-form.h>
+#include <wocky/wocky-namespaces.h>
+
 #include <salut/caps-channel-manager.h>
 
 #include "utils.h"
@@ -33,7 +36,12 @@
 #define DEBUG(msg, ...) \
   g_debug ("%s: " msg, G_STRFUNC, ##__VA_ARGS__)
 
-#define INTERESTED "com.meego.xpmn.ytstenut.Channel/interested/"
+#define CHANNEL_PREFIX "com.meego.xpmn.ytstenut.Channel/"
+#define UID CHANNEL_PREFIX "uid/"
+#define TYPE CHANNEL_PREFIX "type/"
+#define NAME CHANNEL_PREFIX "name/"
+#define CAPS CHANNEL_PREFIX "caps/"
+#define INTERESTED CHANNEL_PREFIX "interested/"
 
 static void channel_manager_iface_init (gpointer g_iface, gpointer data);
 static void caps_channel_manager_iface_init (gpointer g_iface, gpointer data);
@@ -54,30 +62,123 @@ ytst_caps_manager_class_init (YtstCapsManagerClass *klass)
 }
 
 static void
+add_value_to_field (gpointer data,
+    gpointer user_data)
+{
+  WockyNode *field = user_data;
+  WockyNode *tmp;
+  const gchar *content = data;
+
+  tmp = wocky_node_add_child (field, "value");
+  wocky_node_set_content (tmp, content);
+}
+
+static WockyDataForm *
+make_new_data_form (const gchar *uid,
+    const gchar *type,
+    GPtrArray *names,
+    GPtrArray *caps)
+{
+  WockyNode *node, *tmp;
+  WockyDataForm *out;
+  /* the default */
+  const gchar *yts_type = type != NULL ? type : "application";
+  gchar *form_type = g_strdup_printf ("%s%s", SERVICE_PREFIX, uid);
+
+  node = wocky_node_new ("x", WOCKY_XMPP_NS_DATA);
+  wocky_node_add_build (node,
+      '@', "type", "result",
+
+      '(', "field",
+        '@', "var", "FORM_TYPE",
+        '@', "type", "hidden",
+        '(', "value",
+          '$', form_type,
+        ')',
+      ')',
+
+      '(', "field",
+        '@', "var", "type",
+        '(', "value",
+          '$', yts_type,
+        ')',
+      ')',
+
+      NULL);
+
+  g_free (form_type);
+
+  /* do the next two by hand as we'll need to add the values
+   * manually */
+  tmp = wocky_node_add_child (node, "field");
+  wocky_node_set_attribute (tmp, "var", "name");
+  g_ptr_array_foreach (names, add_value_to_field, tmp);
+
+  tmp = wocky_node_add_child (node, "field");
+  wocky_node_set_attribute (tmp, "var", "capabilities");
+  g_ptr_array_foreach (caps, add_value_to_field, tmp);
+
+  out = wocky_data_form_new_from_node (node, NULL);
+
+  wocky_node_free (node);
+
+  return out;
+}
+
+static void
 ytst_caps_manager_represent_client (GabbleCapsChannelManager *manager,
     const gchar *client_name,
     const GPtrArray *filters,
     const gchar * const *cap_tokens,
-    GabbleCapabilitySet *cap_set)
+    GabbleCapabilitySet *cap_set,
+    GPtrArray *data_forms)
 {
   const gchar * const *t;
+
+  WockyDataForm *form;
+  const gchar *uid = NULL;
+  const gchar *yts_type = NULL;
+  GPtrArray *names = g_ptr_array_new ();
+  GPtrArray *caps = g_ptr_array_new ();
 
   for (t = cap_tokens; t != NULL && *t != NULL; t++)
     {
       const gchar *cap = *t;
       gchar *feature;
 
-      if (!g_str_has_prefix (*t, INTERESTED))
-        continue;
-
-      cap = *t + strlen (INTERESTED);
-
-      feature = g_strdup_printf ("%s%s+notify", CAPS_FEATURE_PREFIX, cap);
-
-      gabble_capability_set_add (cap_set, feature);
-
-      g_free (feature);
+      if (g_str_has_prefix (*t, INTERESTED))
+        {
+          cap = *t + strlen (INTERESTED);
+          feature = g_strdup_printf ("%s%s+notify", CAPS_FEATURE_PREFIX, cap);
+          gabble_capability_set_add (cap_set, feature);
+          g_free (feature);
+        }
+      else if (g_str_has_prefix (*t, UID))
+        {
+          uid = *t + strlen (UID);
+        }
+      else if (g_str_has_prefix (*t, TYPE))
+        {
+          yts_type = *t + strlen (TYPE);
+        }
+      else if (g_str_has_prefix (*t, NAME))
+        {
+          g_ptr_array_add (names, (gpointer) (*t + strlen (NAME)));
+        }
+      else if (g_str_has_prefix (*t, CAPS))
+        {
+          g_ptr_array_add (caps, (gpointer) (*t + strlen (CAPS)));
+        }
     }
+
+  if (uid != NULL)
+    {
+      form = make_new_data_form (uid, yts_type, names, caps);
+      g_ptr_array_add (data_forms, form);
+    }
+
+  g_ptr_array_unref (names);
+  g_ptr_array_unref (caps);
 }
 
 static void
---
+++ tests/twisted/salut/hct.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2011 Intel Corp.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+import avahi
+
+from salutservicetest import call_async, EventPattern, assertEquals, \
+    assertLength, assertContains
+from saluttest import exec_test, elem_iq, elem
+from avahitest import AvahiAnnouncer, AvahiListener, txt_get_key, get_host_name
+from xmppstream import connect_to_stream
+import salutconstants as cs
+import ns
+import yconstants as ycs
+
+def test(q, bus, conn):
+    contact_name = "test-hct@" + get_host_name()
+
+    call_async(q, conn.Future, 'EnsureSidecar', ycs.STATUS_IFACE)
+    conn.Connect()
+
+    q.expect('dbus-signal', signal='StatusChanged', args=[0, 0])
+
+    e = q.expect('dbus-return', method='EnsureSidecar')
+    path, props = e.value
+    assertEquals({}, props)
+
+    self_handle = conn.GetSelfHandle()
+    self_handle_name =  conn.InspectHandles(cs.HT_CONTACT, [self_handle])[0]
+
+    AvahiListener(q).listen_for_service("_presence._tcp")
+    e = q.expect('service-added', name = self_handle_name,
+        protocol = avahi.PROTO_INET)
+    service = e.service
+    service.resolve()
+
+    q.expect('service-resolved', service=service)
+
+    # now update the caps
+    conn.ContactCapabilities.UpdateCapabilities([
+        ('well.gnome.name', [],
+         ['com.meego.xpmn.ytstenut.Channel/uid/org.gnome.Banshee',
+          'com.meego.xpmn.ytstenut.Channel/type/application',
+          'com.meego.xpmn.ytstenut.Channel/name/en_GB/Banshee Media Player',
+          'com.meego.xpmn.ytstenut.Channel/name/fr/Banshee Lecteur de Musique',
+          'com.meego.xpmn.ytstenut.Channel/caps/urn:ytstenut:capabilities:yts-caps-audio',
+          'com.meego.xpmn.ytstenut.Channel/caps/urn:ytstenut:data:jingle:rtp'])])
+
+    e, _ = q.expect_many(EventPattern('service-resolved', service=service),
+                         EventPattern('dbus-signal', signal='ServiceAdded'))
+    node = txt_get_key(e.txt, 'node')
+    ver = txt_get_key(e.txt, 'ver')
+
+    # don't check the hash here because it could change in salut and
+    # it would break this test even though it wouldn't /actually/ be
+    # broken
+
+    outbound = connect_to_stream(q, contact_name,
+        self_handle_name, str(e.pt), e.port)
+    e = q.expect('connection-result')
+    assert e.succeeded, e.reason
+    e = q.expect('stream-opened', connection=outbound)
+
+    # ask what this ver means
+    request = \
+        elem_iq(outbound, 'get', from_=contact_name)(
+          elem(ns.DISCO_INFO, 'query', node=(node + '#' + ver))
+        )
+    outbound.send(request)
+
+    # receive caps
+    e = q.expect('stream-iq', connection=outbound,
+                 query_ns=ns.DISCO_INFO, iq_id=request['id'])
+
+    iq = e.stanza
+    query = iq.children[0]
+
+    x = None
+    for child in query.children:
+        if child.name == 'x' and child.uri == ns.X_DATA:
+            # we should only have one child
+            assert x is None
+            x = child
+            # don't break here as we can waste time to make sure x
+            # isn't assigned twice
+
+    assert x is not None
+
+    for field in x.children:
+        if field['var'] == 'FORM_TYPE':
+            assertEquals('hidden', field['type'])
+            assertEquals('urn:ytstenut:capabilities#org.gnome.Banshee',
+                         field.children[0].children[0])
+        elif field['var'] == 'type':
+            assertEquals('application', field.children[0].children[0])
+        elif field['var'] == 'name':
+            names = [a.children[0] for a in field.children]
+            assertLength(2, names)
+            assertContains('en_GB/Banshee Media Player', names)
+            assertContains('fr/Banshee Lecteur de Musique', names)
+        elif field['var'] == 'capabilities':
+            caps = [a.children[0] for a in field.children]
+            assertLength(2, caps)
+            assertContains('urn:ytstenut:capabilities:yts-caps-audio', caps)
+            assertContains('urn:ytstenut:data:jingle:rtp', caps)
+        else:
+            assert False
+
+if __name__ == '__main__':
+    exec_test(test)
