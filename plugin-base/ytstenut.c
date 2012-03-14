3d66374eea1d4729859c11fdf4475679f94577fa
Alvaro Soliverez <alvaro.soliverez@collabora.co.uk>
2012-03-14 11:23:42 -0300

    Fixed salut initialization to use a callback and not calling code that is in salut core and not in the plugin code

--- plugin-base/ytstenut.c
+++ plugin-base/ytstenut.c
@@ -74,13 +74,14 @@ ytst_plugin_class_init (YtstPluginClass *klass)
 #ifdef SALUT
 static void
 ytstenut_plugin_initialize (SalutPlugin *plugin,
-    TpBaseConnectionManager *connection_manager)
+    TpBaseConnectionManager *connection_manager,
+    SalutCreateProtocolImpl callback)
 {
   TpBaseProtocol *protocol;
 
   DEBUG ("%p on connection manager %p", plugin, connection_manager);
 
-  protocol = salut_protocol_new (G_TYPE_NONE,
+  protocol = callback (G_TYPE_NONE,
       "_ytstenut._tcp", "local-ytstenut", "Ytstenut protocol", "im-ytstenut");
   tp_base_connection_manager_add_protocol (connection_manager, protocol);
 }
